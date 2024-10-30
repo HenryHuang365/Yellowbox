e7d4374d6043dff27ea5f369afb5c8521404be60
Jing Huang <jinghuang365@gmail.com>
2024-10-30 17:10:12 +1100

    initialise a server and mockdata to test the implementations

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Load mock data
+const mockData = require('./mockData.json');
+const API_BEARER_TOKEN = `eyJ0eXAiOiJKadsCJhbGciOiJIy45wNiJ9.eyJpc3MiOiJ5ZWx...`
+
+app.use((req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || authHeader !== `Bearer ${API_BEARER_TOKEN}`) {
+    return res.status(403).json({ error: 'Forbidden: Invalid or missing authorization token' });
+  }
+  next();
+});
+
+// This simulates the single request restriction
+let activeRequest = false; 
+// This simulates the max 5 concurrent requests
+let activeRequestsCount = 0;
+
+// The API Allows only 1 request at a time
+app.get('/api/one-request/:deviceId', async (req, res) => {
+  if (activeRequest) {
+    return res.status(429).send({ error: 'Only one request allowed at a time.' });
+  }
+  activeRequest = true;
+
+  const deviceId = req.params.deviceId;
+
+  setTimeout(() => {
+    activeRequest = false;
+    res.json({ online: mockData[deviceId] || false });
+  }, 1000);
+});
+
+// The API Allows unlimited simultaneous requests
+app.get('/api/unlimited-requests/:deviceId', (req, res) => {
+  const deviceId = req.params.deviceId;
+
+  setTimeout(() => {
+    res.json({ online: mockData[deviceId] || false });
+  }, 10000); // Fixed 10 seconds respons time
+});
+
+// Scenario 3: Max 5 simultaneous requests with random delay between 1-3 seconds
+app.get('/api/limited-requests/:deviceId', (req, res) => {
+  if (activeRequestsCount >= 5) {
+    return res.status(429).send({ error: 'Max 5 allowed at a time.' });
+  }
+
+  activeRequestsCount++;
+
+  const deviceId = req.params.deviceId;
+
+  const delay = Math.floor(Math.random() * (3000 - 1000 + 1) + 1000);
+  setTimeout(() => {
+    activeRequestsCount--;
+    res.json({ online: mockData[deviceId] || false });
+  }, delay); // Random 1 to 3 seconds response time
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
